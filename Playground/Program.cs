5a0b987393736aac67d1124fdfe30f48faf96077
Ben Schneider <benscr.email@gmail.com>
2025-12-31 14:37:45 +0100

    Update

--- Playground/Program.cs
+++ Playground/Program.cs
@@ -1,6 +1,18 @@
 ﻿using BenScr.Random;
 
 RandomCS randomCS = new RandomCS();
+
+int count = 0;
+
+for(int i = 0; i < 1000000; i++)
+{
+    count += randomCS.Next<int>(1, 7);
+}
+
+int avg = count /= 1000000;
+
+Console.WriteLine(avg);
+
 Console.WriteLine(randomCS.Next<int>(0, 5));
 Console.WriteLine(randomCS.Next<bool>());
 Console.WriteLine(randomCS.Next<string>());
